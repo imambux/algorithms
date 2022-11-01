391cd99a23b39f6d890a01b17456e67d0d35e7ff
Imam Bux <imam.bux@scout24.com>
2022-11-01 23:11:59 +0100

    Multiple Pointers with less space complexity

--- src/com/imambux/patterns/MultiplePointers.java
+++ src/com/imambux/patterns/MultiplePointers.java
@@ -35,21 +35,17 @@ public class MultiplePointers {
 
   // Less space complexity
   public static int countUniqueNumbers2(int[] arr) {
-    if (arr.length > 1) {
-      int i = 0;
-      for (int j = 1; j <= arr.length; j++) {
-        if (j == arr.length) {
-          return i + 1;
-        }
+    if (arr.length == 0) return 0;
 
-        if (arr[i] != arr[j]) {
-          i++;
-          arr[i] = arr[j];
-        }
+    int i = 0;
+    for (int j = 1; j < arr.length; j++) {
+      if (arr[i] != arr[j]) {
+        i++;
+        arr[i] = arr[j];
       }
     }
 
-    return arr.length;
+    return i + 1;
   }
 
   public static void main(String[] args) {
