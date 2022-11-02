fbac76f8b7f21e3c9f1379b8007001c71cb73d0f
Imam Bux <imam.bux@scout24.com>
2022-11-02 23:37:42 +0100

    Multiple Pointers - more examples

--- src/com/imambux/patterns/MultiplePointers.java
+++ src/com/imambux/patterns/MultiplePointers.java
@@ -1,18 +1,22 @@
 package com.imambux.patterns;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 public class MultiplePointers {
 
   public static int[] sumZero(int[] arr) {
-    int left = 0;
-    int right = arr.length - 1;
+    int leftPointer = 0;
+    int rightPointer = arr.length - 1;
 
-    while (left < right) {
-      if (arr[left] + arr[right] == 0) {
-        return new int[]{arr[left], arr[right]};
-      } else if (arr[left] + arr[right] > 0) {
-        right--;
+    while (leftPointer < rightPointer) {
+      if (arr[leftPointer] + arr[rightPointer] == 0) {
+        return new int[]{arr[leftPointer], arr[rightPointer]};
+      } else if (arr[leftPointer] + arr[rightPointer] > 0) {
+        rightPointer--;
       } else {
-        left++;
+        leftPointer++;
       }
     }
 
@@ -48,10 +52,51 @@ public class MultiplePointers {
     return i + 1;
   }
 
-  public static void main(String[] args) {
-    System.out.println(countUniqueNumbers2(new int[]{1,1,1,1,1,2}));
-    System.out.println(countUniqueNumbers2(new int[]{1,2,3,4,4,4,7,7,12,12,13}));
-    System.out.println(countUniqueNumbers2(new int[]{}));
-    System.out.println(countUniqueNumbers2(new int[]{-2,-1,-1,0,1}));
+  private static boolean areThereDuplicates(int... arr) {
+    int start = 0;
+    int next = 1;
+
+    while (next < arr.length) {
+      if (arr[start] == arr[next]) {
+        return true;
+      }
+
+      start++;
+      next++;
+    }
+
+    return false;
   }
+
+  // Shorter version
+  private static boolean areThereDuplicates2(Integer... arr) {
+    Set<Integer> targetSet = new HashSet<>();
+    Collections.addAll(targetSet, arr);
+
+    return targetSet.size() != arr.length;
+  }
+
+  public static void main(String[] args) {
+    // sumZero
+//    System.out.println(sumZero(new int[]{-3, -2, -1, 0, 1, 2, 3})); // [-3, 3]
+//    System.out.println(sumZero(new int[]{-2, 0, 1, 3})); // []
+
+//    System.out.println(countUniqueNumbers2(new int[]{1,1,1,1,1,2}));
+//    System.out.println(countUniqueNumbers2(new int[]{1,2,3,4,4,4,7,7,12,12,13}));
+//    System.out.println(countUniqueNumbers2(new int[]{}));
+//    System.out.println(countUniqueNumbers2(new int[]{-2,-1,-1,0,1}));
+
+    // areThereDuplicates
+//    System.out.println(areThereDuplicates(1, 2, 3)); // false
+//    System.out.println(areThereDuplicates(1, 2, 2)); // true
+//    System.out.println(areThereDuplicates(1, 2, 4, 5, 1)); // true
+//    System.out.println(areThereDuplicates(1, 2, 4, 5, 0)); // false
+
+    // areThereDuplicates2
+    System.out.println(areThereDuplicates2(1, 2, 3)); // false
+    System.out.println(areThereDuplicates2(1, 2, 2)); // true
+    System.out.println(areThereDuplicates2(1, 2, 4, 5, 1)); // true
+    System.out.println(areThereDuplicates2(1, 2, 4, 5, 0)); // false
+  }
+
 }
