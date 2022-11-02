daee31e8ca183e632062b9cb1918b198dad003a3
Imam Bux <imam.bux@scout24.com>
2022-11-02 22:56:59 +0100

    Frequence Counters - more examples

--- src/com/imambux/patterns/FrequencyCounter.java
+++ src/com/imambux/patterns/FrequencyCounter.java
@@ -1,8 +1,6 @@
 package com.imambux.patterns;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 public class FrequencyCounter {
 
@@ -51,14 +49,60 @@ public class FrequencyCounter {
     return true;
   }
 
+  public static boolean sameFrequency(int x, int y) {
+    Map<Character, Integer> xFC = new HashMap<>();
+    for (char c : (x + "").toCharArray()) {
+      xFC.merge(c, 1, Integer::sum);
+    }
+
+    Map<Character, Integer> yFC = new HashMap<>();
+    for (char c : (y + "").toCharArray()) {
+      yFC.merge(c, 1, Integer::sum);
+    }
+
+    for (char c : xFC.keySet()) {
+      if (xFC.get(c) != yFC.get(c)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  public static boolean areThereDuplicates(Integer... arr) {
+    Map<Integer, Integer> arrFC = new HashMap();
+
+    for (int i = 0; i < arr.length; i++) {
+      if (arrFC.get(arr[i]) == null) {
+        arrFC.put(arr[i], 1);
+      } else {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
   public static void main(String[] args) {
-    System.out.println(validAnagram("", "")); // true
-    System.out.println(validAnagram("aaz", "zza")); // false
-    System.out.println(validAnagram("anagram", "nagaram")); // true
-    System.out.println(validAnagram("rat", "car")); // false
-    System.out.println(validAnagram("awesome", "awesom")); // false
-    System.out.println(validAnagram("qwerty", "qeywrt")); // true
-    System.out.println(validAnagram("texttwisttime", "timetwisttext")); // true
+//    System.out.println(validAnagram("", "")); // true
+//    System.out.println(validAnagram("aaz", "zza")); // false
+//    System.out.println(validAnagram("anagram", "nagaram")); // true
+//    System.out.println(validAnagram("rat", "car")); // false
+//    System.out.println(validAnagram("awesome", "awesom")); // false
+//    System.out.println(validAnagram("qwerty", "qeywrt")); // true
+//    System.out.println(validAnagram("texttwisttime", "timetwisttext")); // true
+
+    // sameFrequency
+//    System.out.println(sameFrequency(182, 281)); // true
+//    System.out.println(sameFrequency(34, 14)); // false
+//    System.out.println(sameFrequency(3589578, 5879385)); // true
+//    System.out.println(sameFrequency(22, 222)); // false
+
+    // areThereDuplicates
+    System.out.println(areThereDuplicates(1, 2, 3)); // false
+    System.out.println(areThereDuplicates(1, 2, 2)); // true
+    System.out.println(areThereDuplicates(1, 2, 4, 5, 1)); // true
+    System.out.println(areThereDuplicates(1, 2, 4, 5, 0)); // false
   }
 
 }
