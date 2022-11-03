d4f46375d19e7b86454f508ed93431e940d66cb1
Imam Bux <imam.bux@scout24.com>
2022-11-03 14:22:43 +0100

    Multiple Pointers - isSubsequence

--- src/com/imambux/patterns/MultiplePointers.java
+++ src/com/imambux/patterns/MultiplePointers.java
@@ -76,6 +76,25 @@ public class MultiplePointers {
     return targetSet.size() != arr.length;
   }
 
+  public static boolean averagePair(int[] arr, double average) {
+    if (arr.length == 0) return false;
+
+    int leftPointer = 0;
+    int rightPointer = arr.length - 1;
+
+    while (leftPointer < rightPointer) {
+      if ((double)(arr[leftPointer] + arr[rightPointer]) / 2 > average) {
+        rightPointer--;
+      } else if ((double)(arr[leftPointer] + arr[rightPointer]) / 2 < average) {
+        leftPointer++;
+      } else {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
   public static void main(String[] args) {
     // sumZero
 //    System.out.println(sumZero(new int[]{-3, -2, -1, 0, 1, 2, 3})); // [-3, 3]
@@ -93,10 +112,38 @@ public class MultiplePointers {
 //    System.out.println(areThereDuplicates(1, 2, 4, 5, 0)); // false
 
     // areThereDuplicates2
-    System.out.println(areThereDuplicates2(1, 2, 3)); // false
-    System.out.println(areThereDuplicates2(1, 2, 2)); // true
-    System.out.println(areThereDuplicates2(1, 2, 4, 5, 1)); // true
-    System.out.println(areThereDuplicates2(1, 2, 4, 5, 0)); // false
+//    System.out.println(areThereDuplicates2(1, 2, 3)); // false
+//    System.out.println(areThereDuplicates2(1, 2, 2)); // true
+//    System.out.println(areThereDuplicates2(1, 2, 4, 5, 1)); // true
+//    System.out.println(areThereDuplicates2(1, 2, 4, 5, 0)); // false
+
+    // averagePair
+//    System.out.println(averagePair(new int[]{1,2,3},2.5));; // true
+//    System.out.println(averagePair(new int[]{1,3,3,5,6,7,10,12,19},8));; // true
+//    System.out.println(averagePair(new int[]{-1,0,3,4,5,6}, 4.1));; // false
+//    System.out.println(averagePair(new int[]{},4));; // false
+
+    // isSubsequence
+    System.out.println(isSubsequence("hello", "hello world")); // true
+    System.out.println(isSubsequence("sing", "sting")); // true
+    System.out.println(isSubsequence("abc", "abracadabra")); // true
+    System.out.println(isSubsequence("abc", "acb")); // false (order matters)
+  }
+
+  private static boolean isSubsequence(String s, String t) {
+    if (s.length() == 0) return true;
+
+    int sPointer = 0;
+    for (char c : t.toCharArray()) {
+     if (c == s.charAt(sPointer)) {
+       sPointer++;
+     }
+     if (sPointer == s.length()) {
+       return true;
+     }
+    }
+
+    return false;
   }
 
 }
